0ccff4fd4632e34ad7d308f7aae475f2801e96c5
jaeseong-kim <doctorwho123@naver.com>
2023-05-21 19:34:49 +0900

    '2023.05.21'

---
+++ ssia-ch2-ex5/src/main/java/com/example/ssiach2ex5/config/Config.java
@@ -0,0 +1,28 @@
+package com.example.ssiach2ex5.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+public class Config extends WebSecurityConfigurerAdapter {
+
+  @Autowired
+  CustomAuthenticationProvider authenticationProvider;
+
+  @Override
+  protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+    auth.authenticationProvider(authenticationProvider);
+  }
+
+  @Override
+  protected void configure(HttpSecurity http) throws Exception {
+    http.httpBasic();
+
+    http.authorizeRequests()
+        .anyRequest()
+        .authenticated();
+  }
+}
