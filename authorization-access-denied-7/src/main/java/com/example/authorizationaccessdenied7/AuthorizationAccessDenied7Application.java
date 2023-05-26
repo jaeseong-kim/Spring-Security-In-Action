42591b4701a49b0d9de8b16dd1928ead1e1d5d93
jaeseong-kim <doctorwho123@naver.com>
2023-05-26 14:44:14 +0900

    2023.05.26 ch.7 finished

---
+++ authorization-access-denied-7/src/main/java/com/example/authorizationaccessdenied7/AuthorizationAccessDenied7Application.java
@@ -0,0 +1,13 @@
+package com.example.authorizationaccessdenied7;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuthorizationAccessDenied7Application {
+
+  public static void main(String[] args) {
+    SpringApplication.run(AuthorizationAccessDenied7Application.class, args);
+  }
+
+}
---
+++ authorization-access-denied-7/src/test/java/com/example/authorizationaccessdenied7/AuthorizationAccessDenied7ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.authorizationaccessdenied7;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthorizationAccessDenied7ApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
